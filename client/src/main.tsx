9373346b7f723885af1c35bee64c837eebcc27a2
Vansh <143195512+Vansh-Raikwar@users.noreply.github.com>
2026-06-01 18:51:56 +0530

    remove undefined keyword

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")!).render(
             <AppProvider>
                 <App />
             </AppProvider>
-            <Toaster richColors position="bottom-right" />
+            <Toaster position="bottom-right" />
         </ThemeProvider>
     </BrowserRouter>
 );
